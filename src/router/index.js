2bf334aaba5977b81ea562ddf27bdc39912e4604
kongkong1202 <1210269464@qq.com>
2020-01-08 01:25:11 +0800

    '底部导航'

--- src/router/index.js
+++ src/router/index.js
@@ -48,19 +48,19 @@ const router = new VueRouter({
   routes
 });
 
-router.beforeEach((to,from,next)=>{
-  if(to.matched.some( r => r.meta.requireAuth )){
-    if(store.state.token){
-      next();
-    }else{
-      next({
-        path:'/login',
-        query:{redirect:to.fullPath}
-      })
-    }
-  }else{
-    next();
-  }
-});
+// router.beforeEach((to,from,next)=>{
+//   if(to.matched.some( r => r.meta.requireAuth )){
+//     if(store.state.token){
+//       next();
+//     }else{
+//       next({
+//         path:'/login',
+//         query:{redirect:to.fullPath}
+//       })
+//     }
+//   }else{
+//     next();
+//   }
+// });
 export default router;
 
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,29 +1,27 @@
 <template>
   <div class="home">
-    <button @click="logout">退出</button>
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="user-info">
+      <span>2019年11月6日</span>
+      <van-image round width="10rem" height="10rem" src="https://img.yzcdn.cn/vant/cat.jpeg"/>
+      <span>管理层-Jack</span>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-import * as types from '../store/types';
+import * as types from "../store/types";
 
 export default {
-  name: 'home',
-  components: {
-    HelloWorld
-  },
-  methods:{
-    async logout(){
+  name: "home",
+  components: {},
+  methods: {
+    async logout() {
       console.log("logout");
       this.$store.commit(types.LOGOUT);
       this.$router.push({
-        path:'login',
-      })
+        path: "login"
+      });
     }
   }
-}
+};
 </script>
